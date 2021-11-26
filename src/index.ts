a889f283d54df34f53310f35318641d6bd827ead
Jun Ito <jujunjun110@gmail.com>
2021-11-26 19:41:34 +0900

    Update: error handling

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,25 @@
 import { AFrame } from "aframe";
 import moduleFunction from "./modules/module";
-
-console.log(moduleFunction("Hello World!"));
-const scene = document.getElementsByTagName("a-scene")[0] as any;
-const camera = scene.camera;
-const area = document.getElementById("logger")!;
-
-setInterval(() => {
+function main() {
+  const area = document.getElementById("logger")!;
   try {
-    console.log(camera.position);
-    const pos = camera.position;
-    const txt = `
+    console.log(moduleFunction("Hello World!"));
+
+    const scene = document.getElementsByTagName("a-scene")[0] as any;
+    const camera = scene.camera;
+
+    setInterval(() => {
+      console.log(camera.position);
+      const pos = camera.position;
+      const txt = `
         ${area}\n
         ${camera}\n
         x: ${pos.x}
     `;
-    area.innerText = txt;
+      area.innerText = txt;
+    }, 1000);
   } catch (e) {
     const _e = e as Error;
-    area.innerText = _e.message;
   }
-}, 1000);
+}
+main();
