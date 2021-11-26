9d3ede4ac4fbb97f2fa5b1d1e726ec2915e35e31
Jun Ito <jujunjun110@gmail.com>
2021-11-26 19:15:23 +0900

    Update: Logger

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
+import { AFrame } from "aframe";
 import moduleFunction from "./modules/module";
 
 console.log(moduleFunction("Hello World!"));
+const scene = document.getElementsByTagName("a-scene")[0] as any;
+const camera = scene.camera;
+const area = document.getElementById("logger")!;
+
+setInterval(() => {
+  console.log(camera.position);
+  const pos = camera.position;
+  const txt = `x: ${pos.x}`;
+  area.innerText = txt;
+}, 1000);
